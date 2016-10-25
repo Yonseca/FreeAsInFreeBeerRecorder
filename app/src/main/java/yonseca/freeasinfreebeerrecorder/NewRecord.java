d2b6f271679392570a93c1029df4471100e20781
Alejandro Alejandro S <yonseca@gmail.com>
2016-10-25 20:24:28 +0200

    Starting with MediaRecorder

--- app/src/main/java/yonseca/freeasinfreebeerrecorder/NewRecord.java
+++ app/src/main/java/yonseca/freeasinfreebeerrecorder/NewRecord.java
@@ -1,13 +1,34 @@
 package yonseca.freeasinfreebeerrecorder;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
 public class NewRecord extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        micOn();
         setContentView(R.layout.activity_new_record);
+
+    }
+
+    /*
+     * @return A brand new MediaRecorded instance ready to be used.
+     */
+    private MediaRecorder micOn() {
+        MediaRecorder mic = new MediaRecorder();
+        int audioSourceMax = MediaRecorder.getAudioSourceMax();
+        if (audioSourceMax > 0) {
+            mic.setAudioSource(MediaRecorder.AudioSource.MIC);
+        }
+        mic.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); // 3GP audio format
+        mic.setAudioEncoder(MediaRecorder.AudioEncoder.VORBIS); // OGG Vorbis
+
+
+        return mic;
     }
 }
---
+++ app/src/main/java/yonseca/freeasinfreebeerrecorder/Settings/RecordingSettings.java
@@ -0,0 +1,14 @@
+package yonseca.freeasinfreebeerrecorder.Settings;
+
+import android.media.MediaRecorder;
+
+/**
+ * Created by yonseca on 25/10/16.
+ */
+
+public final class RecordingSettings {
+
+    // public static Uri =
+    public static int audioEncoder = MediaRecorder.AudioEncoder.VORBIS;
+    public static int outputFormat = MediaRecorder.OutputFormat.THREE_GPP;
+}
