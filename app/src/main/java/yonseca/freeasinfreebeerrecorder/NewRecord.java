c330525dcd4f70cdce1dff6d51ccf3a38dfbd2e3
Alejandro S <yonseca@gmail.com>
2016-11-04 00:09:41 +0100

    Fails :(

--- app/src/main/java/yonseca/freeasinfreebeerrecorder/NewRecord.java
+++ app/src/main/java/yonseca/freeasinfreebeerrecorder/NewRecord.java
@@ -3,6 +3,7 @@ package yonseca.freeasinfreebeerrecorder;
 import android.content.SharedPreferences;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -26,10 +27,28 @@ public class NewRecord extends AppCompatActivity {
         startButton = (Button) findViewById(R.id.button);
         stopButton = (Button) findViewById(R.id.button2);
         preferences = getSharedPreferences("Settings", MODE_PRIVATE);
-        preferences.edit().putString("filePath", getFilesDir().getAbsolutePath() + "/" + date.get(Calendar.YEAR) + date.get(Calendar.MONTH) + date.get(Calendar.DAY_OF_MONTH) + date.get(Calendar.HOUR_OF_DAY) + date.get(Calendar.MINUTE) + ".ogg");
+        preferences.edit().putString("filePath", getFullFilePath());
         micOn();
         setContentView(R.layout.activity_new_record);
 
+    }
+
+
+    /**
+     * @return Full file path. Should end up with something like YYYYmmddHHMM.ogg
+     */
+    @NonNull
+    private String getFullFilePath() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getFilesDir().getAbsolutePath())
+                .append(date.get(Calendar.YEAR))
+                .append(date.get(Calendar.MONTH))
+                .append(date.get(Calendar.DAY_OF_MONTH))
+                .append(date.get(Calendar.HOUR_OF_DAY))
+                .append(date.get(Calendar.MINUTE))
+                .append(".ogg");
+
+        return sb.toString();
     }
 
     /*
@@ -43,7 +62,7 @@ public class NewRecord extends AppCompatActivity {
             mic.setAudioSource(MediaRecorder.AudioSource.MIC);
             mic.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); // 3GP audio format
             mic.setAudioEncoder(MediaRecorder.AudioEncoder.VORBIS); // OGG Vorbis
-            mic.setOutputFile(preferences.getString("filePath", ""));
+            mic.setOutputFile(preferences.getString("filePath", null));
 
 
             mic.prepare();
@@ -59,8 +78,6 @@ public class NewRecord extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     mic.stop();
-
-                    mic.release();
                 }
             });
 
@@ -69,5 +86,29 @@ public class NewRecord extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        mic.release();
+        super.onPause();
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        micOn();
+        super.onResume();
     }
 }
